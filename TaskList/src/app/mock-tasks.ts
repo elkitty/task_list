51b6ef8bfa55c23948f0750f465de6948fd36497
elkitty <hablaconwafle@gmail.com>
2022-11-12 20:26:18 -0300

    lista de items

--- TaskList/src/app/mock-tasks.ts
+++ TaskList/src/app/mock-tasks.ts
@@ -3,28 +3,28 @@ import {Task} from "./Task"
 export const TASKS: Task[] = [
   {
     id: 1,
-    text: "terminar tarea 1",
+    text: "Terminar tarea 1",
     day: "11 de enero",
     reminder: true,
 
   },
   {
     id: 2,
-    text: "terminar tarea 2",
+    text: "Terminar tarea 2",
     day: "12 de enero",
     reminder: true,
 
   },
   {
     id: 3,
-    text: "terminar tarea 3",
+    text: "Terminar tarea 3",
     day: "13 de enero",
     reminder: true,
 
   },
   {
     id: 4,
-    text: "terminar tarea 4",
+    text: "Terminar tarea 4",
     day: "14 de enero",
     reminder: false,
 
